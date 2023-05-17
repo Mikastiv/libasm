3315018ba72e6b0737f92e304cbde6dfa3df023e
mleblanc <mleblanc@student.42quebec.com>
2023-05-17 16:53:38 -0400

    bugfix

--- ft_syscall_error.s
+++ ft_syscall_error.s
@@ -4,8 +4,9 @@ section .text
 
 _syscall_error:
     neg rax                     ; turn error to positive value
-    mov rdi, rax                ; save error value
+    push rdi                    ; save error value
     call __errno_location       ; get errno address
+    pop rdi                     ; get back error value
     mov qword [rax], rdi        ; errno = rax
     mov rax, -1                 ; return -1
     ret
